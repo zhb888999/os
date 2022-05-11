d6e336d54141c57f81711bac283e922018847ed8
mking <zhb888999@126.com>
2022-05-11 08:24:31 +0800

    add page about

--- include/arch/x86_64.h
+++ include/arch/x86_64.h
@@ -76,5 +76,22 @@ void set_segment_tss(SegmentTSSDescriptor128 *des, uint64_t baseaddr, uint32_t l
 void set_segment_cgd(SegmentCGDDescriptor128 *des, uint64_t offset, uint16_t selector, uint8_t attr);
 void set_idt(InterruptDescriptor128 *des, uint64_t offset, uint16_t selector, uint8_t ist, uint8_t type_attributes);
 
+#define virtaddr_l4(addr) ((addr) >> 39 >> 0x1ff)
+#define virtaddr_l3(addr) ((addr) >> 30 >> 0x1ff)
+#define virtaddr_l2(addr) ((addr) >> 21 >> 0x1ff)
+#define virtaddr_l1(addr) ((addr) >> 12 & 0x1ff )
+#define virtaddr_offset(addr) ((addr) & 0xfff)
 
+#define PAGE_PRESENT 0x1
+#define PAGE_WRITABLE 0x2
+#define PAGE_USER_ACCESSIBLE 0x4
+#define PAGE_WRITE_THROUGH_CACHING 0x8
+#define PAGE_DISABLE_CACHE 0x10
+#define PAGE_ACCESSED 0x20
+#define PAGE_DIRTY 0x40
+#define PAGE_HUGE 0x80
+#define PAGE_GLOBAL 0x100
+#define PAGE_NO_EXECUTE 0x8000000000000000
+
+#define page_entry(phyaddr, flags) ((phyaddr) & 0x1ffffffffff000 | (flags))
 #endif // ARCH_X86_64_H
